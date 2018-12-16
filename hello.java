0b336722ee6904f13ec6cfde285db92aec8dcb71
sayedme120 <sayedme120@gmail.com>
2018-12-16 21:55:06 +0600

    Add new line

--- hello.java
+++ hello.java
@@ -1,9 +1,10 @@
 public class Hello{
 	public static void main(String[] args) {
 	  System.out.println("vs code is Awesome...");	
+		  greeting("Md Abu Sayed")
 	}
 
-	public static void display(String name){
+	public static void greeting(String name){
 		System.out.println("welcome " + name);
 	}
 }
