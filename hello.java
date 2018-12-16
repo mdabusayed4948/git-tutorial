5556187f26276e3c8d97945200f7e78289e5e2c4
sayedme120 <sayedme120@gmail.com>
2018-12-16 20:28:37 +0600

    Added a new method called display to Hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Hello{
 	public static void main(String[] args) {
-	  System.out.println("vs code is Awesome...")	
+	  System.out.println("vs code is Awesome...");	
+	}
+
+	public static void display(String name){
+		System.out.println("welcome " + name);
 	}
 }
